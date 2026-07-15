0c65bebaa5490006dc5a4e24dbf3ac18d9a097c9
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-07-15 16:44:35 +0530

    Time: 13 ms (34.82%), Space: 47.6 MB (65.29%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,16 +2,19 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         int dup = 0;
-        int miss = 0;
+        int sum = 0;
         for(int i : nums){
+            sum += i;
             if(set.contains(i)) dup = i;
             set.add(i);
         }
-        for(int i = 1; i <=set.size()+1; i++){
-            if(!set.contains(i)){
-                miss = i;
-            }
-        }
+        int n = nums.length;
+        int miss = (n*(n+1)/2)+dup-sum;
+        // for(int i = 1; i <=set.size()+1; i++){
+        //     if(!set.contains(i)){
+        //         miss = i;
+        //     }
+        // }
         return new int[]{dup,miss};
     }
 }
