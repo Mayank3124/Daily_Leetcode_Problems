ff5d20cd3caff6c58ccc55a9bf4faec6641f3932
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-07-17 22:05:05 +0530

    Time: 3 ms (49.3%), Space: 45.1 MB (18.77%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         for(int i : nums){
             pq.offer(i);
-            if(pq.size() > 2) pq.poll();
         }
 
         return (pq.poll()-1)*(pq.poll()-1);
