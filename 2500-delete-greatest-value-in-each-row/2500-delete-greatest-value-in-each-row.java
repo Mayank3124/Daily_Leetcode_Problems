f448639b510ead71f1eedd98d046b7b413eea5c2
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-07-18 19:28:49 +0530

    Time: 10 ms (13.02%), Space: 46.3 MB (90.16%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                pq.offer(grid[i][j]);
+
+            }
+            for(int j = 0; j < n; j++){
+                grid[i][j] = pq.poll(); 
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            int max = 0;
+            for(int j = 0; j < m; j++){
+                max = Math.max(max, grid[j][i]);
+            }
+            ans += max;
+        }
+        
+        return ans;
+    }
+}
