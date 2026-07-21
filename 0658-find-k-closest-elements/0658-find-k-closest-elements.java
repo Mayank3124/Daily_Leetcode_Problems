13d4371a6f22a886080dfea3b0d2810bc818d4fb
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-07-21 17:55:42 +0530

    Time: 39 ms (23.44%), Space: 48 MB (36.1%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,24 +1,47 @@
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(
+//                 (a, b) -> {
+//                     if (Math.abs(x - a) != Math.abs(x - b)) {
+//                         return Math.abs(x - a) - Math.abs(x - b);
+//                     }
+//                     return a - b;
+//                 });
+//         for (int i : arr) {
+//             pq.add(i);
+   
+//         }
+//         List<Integer> list = new ArrayList<>();
+//         PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+//         while (k-- > 0) {
+//             pq2.offer(pq.poll());
+//         }
+//         while (!pq2.isEmpty()) {
+//             list.add(pq2.poll());
+//         }
+//         return list;
+
+//     }
+// }
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-                (a, b) -> {
-                    if (Math.abs(x - a) != Math.abs(x - b)) {
-                        return Math.abs(x - a) - Math.abs(x - b);
-                    }
-                    return a - b;
-                });
-        for (int i : arr) {
-            pq.add(i);   
-        }
-        List<Integer> list = new ArrayList<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-        while (k-- > 0) {
-            pq2.offer(pq.poll());
-        }
-        while (!pq2.isEmpty()) {
-            list.add(pq2.poll());
-        }
-        return list;
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (Math.abs(x - a) != Math.abs(x - b))
+                    return Math.abs(x - b) - Math.abs(x - a);
+                return b - a;
+            });
+
+        for (int num : arr) {
+            pq.offer(num);
+            if (pq.size() > k)
+                pq.poll();
+        }
+
+        List<Integer> ans = new ArrayList<>(pq);
+        Collections.sort(ans);
+        return ans;
     }
 }
