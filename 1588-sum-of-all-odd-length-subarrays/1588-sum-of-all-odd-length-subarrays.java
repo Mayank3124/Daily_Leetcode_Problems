9bb1393a95f36328660166ffad4a0aaf3f1372b5
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-08-07 16:33:14 +0530

    Time: 2 ms (61.97%), Space: 43.1 MB (43.2%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -5,8 +5,9 @@ class Solution {
         pref[0] = 0;
         for(int i = 1; i < pref.length; i++){
             pref[i] = pref[i-1] + arr[i-1];
+            sum += arr[i-1];
         }
-        for(int i = 1; i <= arr.length; i = i + 2 ){
+        for(int i = 3; i <= arr.length; i = i + 2 ){
             for(int j = i; j < pref.length; j++){
                 sum += (pref[j]-pref[j-i]);
             }
