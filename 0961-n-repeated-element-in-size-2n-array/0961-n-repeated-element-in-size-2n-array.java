71f05bfc225687d3c505e24f70332d87bad088e6
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-08-13 13:54:33 +0530

    Time: 17 ms (25.55%), Space: 47.3 MB (80.53%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -4,7 +4,10 @@ class Solution {
         int n = nums.length;
         for(int i = 0; i < n; i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])==n/2) return nums[i];
+            
+        }
+        for(int k : map.keySet()){
+            if(map.get(k)==n/2) return k;
         }
         return 1;
     }
