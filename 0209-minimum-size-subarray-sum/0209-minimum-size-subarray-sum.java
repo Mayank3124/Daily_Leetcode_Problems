0e3318bae7fb498fb6854d345d205c0308d08b39
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-08-15 11:46:26 +0530

    Time: 1 ms (99.92%), Space: 69.4 MB (26.66%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,30 +1,21 @@
 class Solution {
-    public boolean isGreaterSum(int k, int[] nums, int target){
-        if(nums[k-1] >= target ) return true;
-        for(int i = k; i < nums.length; i++){
-            if(nums[i]-nums[i-k] >= target) return true;
-        }
-        return false;
-    }
     public int minSubArrayLen(int target, int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i]>=target) return 1;
-            nums[i] = nums[i] + nums[i-1];
-        }
-        int l = 1;
-        int r = nums.length;
-        int min = 0;
+        int sum = 0;
+        int l = 0;
+        int Length = 0;
+        int min = Integer.MAX_VALUE; 
+        for(int i = 0; i < nums.length; i++ ){
+            sum += nums[i];
+            Length ++;
 
-        while(l <= r){
-            int mid = l + (r - l)/2;
-            if(isGreaterSum(mid,nums,target)){
-                min = mid;
-                r = mid-1;
-            } else {
-                l = mid + 1;
-            }
+            while(sum >= target){
+                min = Math.min(Length,min);
+                sum -= nums[l];
+                l++;
+                Length--;
+            } 
         }
-        
+        if(min==Integer.MAX_VALUE) return 0;
         return min;
-   }
+    }
 }
