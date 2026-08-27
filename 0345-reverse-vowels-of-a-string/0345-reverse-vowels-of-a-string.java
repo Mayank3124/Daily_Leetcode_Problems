dea9fb920a784ebca638ebc8de3cf6dad9925a6a
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-08-27 15:29:49 +0530

    Time: 3 ms (66.34%), Space: 46.5 MB (79.63%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,21 +1,25 @@
 class Solution {
-    public boolean isVowel(char ch){
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' );
+    public boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
     }
+
     public String reverseVowels(String s) {
         int l = 0;
-        int r = s.length() -1;
-        char str[] = s.toCharArray(); 
+        int r = s.length() - 1;
+        char str[] = s.toCharArray();
 
-        while(l < r){
-            if(isVowel(str[l]) && isVowel(str[r])){
-                char temp = str[l];
-                str[l] = str[r];
-                str[r] = temp; 
-                l++;
-                r--;
-            }else if(!isVowel(str[l])) l++;
-            else if(!isVowel(str[r])) r--;
+        while (l < r) {
+            while(l < r && !isVowel(str[l])) l++;
+            while(l < r && !isVowel(str[r])) r--;
+
+
+            char temp = str[l];
+            str[l] = str[r];
+            str[r] = temp;
+            l++;
+            r--;
+
         }
         return new String(str);
     }
