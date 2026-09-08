131baa7899d94b5e5ffeec9bfcabf4747a4a8ca1
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-09-08 22:35:49 +0530

    Time: 3 ms (33.9%), Space: 49.1 MB (11.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,16 @@
 class Solution {
-    public void Helper(String s, String digits, int idx, HashMap map, List list){
+    public void Helper(String s, String digits, int idx, String[] map, List list){
         if(s.length() == digits.length()) {
             list.add(s);
             return;
         }
-        String str = map.get(digits.charAt(idx)).toString();
+        String str = map[digits.charAt(idx)-'0'];
         for(char ch : str.toCharArray()){
             Helper(s+ch,digits,idx+1,map,list);
         }
     }
     public List<String> letterCombinations(String digits) {
-        HashMap<Character, String> map = new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
+        String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
         List<String> list = new ArrayList<>();
 
