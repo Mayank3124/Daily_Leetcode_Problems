21cb1d2214650de079ceaaa78abb757371b7136d
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-09-12 16:43:23 +0530

    Time: 14 ms (50.09%), Space: 46.5 MB (35.44%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -14,7 +14,7 @@ class Solution {
                     cnt--;
                 }
             }
-            if(cnt == k) return k;
+            
             max = Math.max(max,cnt);
             
         }
