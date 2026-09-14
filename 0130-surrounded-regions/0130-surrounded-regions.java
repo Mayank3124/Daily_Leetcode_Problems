93950f1bb5baa066c2bb021f9288a8895babf975
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-09-14 14:27:25 +0530

    Time: 2 ms (75.4%), Space: 47.5 MB (92.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -14,21 +14,7 @@ class Solution {
             }
         }
     }
-    public void helper2(char[][] board, int i,int j){
-        int m = board.length;
-        int n = board[0].length;
-
-        board[i][j] = 'X';
-
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
-        for(int[] dir : dirs){
-            int newM = i + dir[0];
-            int newN = j + dir[1];
-            if(newM >= 0 && newM < m && newN >= 0 && newN < n && board[newM][newN]=='O'){
-                helper2(board,newM,newN);
-            }
-        }
-    }
+    
     public void solve(char[][] board) {
         boolean[][] check = new boolean[board.length][board[0].length];
 
@@ -53,7 +39,7 @@ class Solution {
         for(int i = 1; i < board.length-1; i++){
             for(int j = 1; j < board[0].length-1; j++){
                 if(!check[i][j] && board[i][j] == 'O'){
-                    helper2(board,i,j);
+                    board[i][j] = 'X';
                 }
             }
         }
