cc5a3f6ab1c08d01a6455f662d570acd7b075251
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-09-14 16:35:39 +0530

    Time: 2 ms (59.5%), Space: 46.3 MB (61.89%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -4,7 +4,6 @@ class Solution {
 
         for(int[] trip : trips){
             int pass = trip[0];
-            if(pass > capacity) return false;
             fre[trip[1]] += pass;
             fre[trip[2]] -= pass;
         }
