50e1227f76435b4e5154e0c5e159dd6a7e9d31c4
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2026-09-20 20:13:22 +0530

    Time: 1 ms (100%), Space: 44.4 MB (10.65%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        int i = 1;
+        for(char ch : s.toCharArray()){
+            sum += (('z' - ch ) + 1)*i;
+            i++; 
+        }
+        return sum;
+    }
+}
